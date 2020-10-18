6efa52fe9d683c3f1d90a7b80238e4030e3304ff
carlosvldz <me@carlosvldz.com>
2020-10-17 23:59:14 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use(require('./routes/index'));
+
+app.listen(3000);
+console.log('Server on port 3000');
